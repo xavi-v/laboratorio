7d42d6ab790a8e2f8dfc38a158729d6183ed02c1
Xavi V <javivergara@udec.cl>
2023-07-24 21:34:45 -0400

    Con contador de visitas 21:34pm

--- laboratorio/views.py
+++ laboratorio/views.py
@@ -16,7 +16,7 @@ def v_laboratorio_create(request):
 
 	return render(request, 'laboratorio_create.html', context)
 ##
-###-----------------------------------------------------------
+# ##-----------------------------------------------------------
 def v_laboratorio_edit(request, laboratorio_id):
 	try:
 		context = {
@@ -30,20 +30,28 @@ def v_laboratorio_edit(request, laboratorio_id):
 		if form.is_valid():
 			form.save()
 			return HttpResponseRedirect("/")
-	#context ya existe
+##	#context ya existe
 	context['form'] = LaboratorioForm(instance=context['labo'])
 	return render(request, 'laboratorio_edit.html', context)
 ##
-###-----------------------------------------------------------
+# ##-----------------------------------------------------------
 def v_laboratorio_list(request):
 	from django.contrib.sessions.models import Session
 	context = {
+			  ##'nvisitas': Session.objects.all().count(), para cuando es contar con inicios de sesion
+			  'nvisitas': 0,
 			  'listar': Laboratorio.objects.all()
 	}
+
+	nvisits = request.session.get('num_visits', 0) #consulta
+	request.session['num_visits'] = nvisits + 1 # guarda variable de session
+	context['nvisitas'] = nvisits + 1 #muestra la info en html
+
+
 	return render(request, 'laboratorio_list.html', context)
 
 ##
-###-----------------------------------------------------------
+# ##-----------------------------------------------------------
 def v_laboratorio_delete(request, laboratorio_id):
 	contchesu = {
 		'lav':Laboratorio.objects.get(id = laboratorio_id)
